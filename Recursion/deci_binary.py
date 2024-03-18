6d6b92b71e167910b9cfc7db0cb980fdaec8c826
Biswadeep Mukhopadhyay <biswadeep.mukhopadhyay00@gmail.com>
2024-03-18 13:47:28 +0530

    Initial

--- Recursion/deci_binary.py
+++
@@ -1,9 +0,0 @@
-def deci_binary(deci):
-    if deci == 0:
-        return 0
-    else:
-        return deci%2 + 10*deci_binary(deci//2)
-
-
-deci = int(input("Enter a number: "))
-print("Binary: ",deci_binary(deci))
